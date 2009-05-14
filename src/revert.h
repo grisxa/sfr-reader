30f5f87eb19818cf7d326cdacce51abddcd5683a
Grigory Batalov <bga@altlinux.org>
2009-05-15 01:26:58 +0400

    Define revert_l(), revert_s() as just empty directive on little-endian to avoid "warning: statement with no effect" message.

--- src/revert.h
+++ src/revert.h
@@ -56,8 +56,8 @@
 #  define revert_l(a) { uint32_t x = le32toh(a); a = x; }
 #  define revert_s(a) { uint16_t x = le16toh(a); a = x; }
 #else
-# define revert_l(a) (a)
-# define revert_s(a) (a)
+# define revert_l(a)
+# define revert_s(a)
 #endif
 void revert_title (struct Title *);
 void revert_offsets (struct Offsets *);
